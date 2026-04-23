2abc7b70a2182e02b3b247c4b4a681e31f772748
KaisarCode <kaisar@kaisarcode.com>
2026-04-23 15:54:26 -0300

    refactor: abstract memory mapping into cross-platform helper functions and improve hash function for index insertions

--- libkrow.c
+++ libkrow.c
@@ -10,6 +10,12 @@
 #ifndef _WIN32
 #define _POSIX_C_SOURCE 200809L
 #define _XOPEN_SOURCE 700
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#else
+#include <windows.h>
 #endif
 
 #include "krow.h"
@@ -18,10 +24,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
 
 typedef struct {
     uint64_t key;
@@ -37,9 +39,19 @@ typedef struct {
     uint64_t data_tail;
 } kc_krow_header_t;
 
-struct kc_krow {
+typedef struct {
+#ifndef _WIN32
     int fd;
-    size_t map_size;
+#else
+    HANDLE file;
+    HANDLE map;
+#endif
+    void *ptr;
+    size_t size;
+} kc_map_t;
+
+struct kc_krow {
+    kc_map_t map;
     kc_krow_header_t *header;
     kc_krow_node_t *index;
     uint8_t *heap;
@@ -56,6 +68,183 @@ static void kc_krow_die(const char *msg) {
 }
 
 /**
+ * Open memory map abstraction.
+ * @param m Map context.
+ * @param path File path.
+ * @param size Requested size.
+ * @param is_new Flag if new file.
+ * @return Status code.
+ */
+static int kc_map_open(kc_map_t *m, const char *path, size_t size, int is_new) {
+#ifndef _WIN32
+    m->fd = open(path, O_RDWR | (is_new ? O_CREAT : 0), 0644);
+    if (m->fd == -1) {
+        return -1;
+    }
+
+    if (is_new && size > 0) {
+        if (ftruncate(m->fd, size) == -1) {
+            return -1;
+        }
+    }
+
+    m->size = size;
+    m->ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, m->fd, 0);
+    if (m->ptr == MAP_FAILED) {
+        return -1;
+    }
+
+    return 0;
+#else
+    DWORD size_high;
+    DWORD size_low;
+
+    m->file = CreateFileA(path, GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+        is_new ? OPEN_ALWAYS : OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (m->file == INVALID_HANDLE_VALUE) {
+        return -1;
+    }
+
+    size_high = (DWORD)(size >> 32);
+    size_low = (DWORD)(size & 0xFFFFFFFF);
+
+    m->map = CreateFileMappingA(m->file, NULL, PAGE_READWRITE,
+        size_high, size_low, NULL);
+    if (!m->map) {
+        CloseHandle(m->file);
+        return -1;
+    }
+
+    m->ptr = MapViewOfFile(m->map, FILE_MAP_ALL_ACCESS, 0, 0, size);
+    if (!m->ptr) {
+        CloseHandle(m->map);
+        CloseHandle(m->file);
+        return -1;
+    }
+
+    m->size = size;
+    return 0;
+#endif
+}
+
+/**
+ * Sync memory map.
+ * @param m Map context.
+ * @return Status code.
+ */
+static int kc_map_sync(kc_map_t *m) {
+#ifndef _WIN32
+    if (msync(m->ptr, m->size, MS_SYNC) == -1) {
+        return -1;
+    }
+    return 0;
+#else
+    if (!FlushViewOfFile(m->ptr, m->size)) {
+        return -1;
+    }
+    return 0;
+#endif
+}
+
+/**
+ * Close memory map.
+ * @param m Map context.
+ * @return None.
+ */
+static void kc_map_close(kc_map_t *m) {
+#ifndef _WIN32
+    if (m->ptr && m->ptr != MAP_FAILED) {
+        munmap(m->ptr, m->size);
+    }
+    if (m->fd != -1) {
+        close(m->fd);
+    }
+#else
+    if (m->ptr) {
+        UnmapViewOfFile(m->ptr);
+    }
+    if (m->map) {
+        CloseHandle(m->map);
+    }
+    if (m->file && m->file != INVALID_HANDLE_VALUE) {
+        CloseHandle(m->file);
+    }
+#endif
+}
+
+/**
+ * Resize memory map.
+ * @param m Map context.
+ * @param new_size New size.
+ * @return Status code.
+ */
+static int kc_map_resize(kc_map_t *m, size_t new_size) {
+#ifndef _WIN32
+    void *new_ptr;
+
+    if (ftruncate(m->fd, new_size) == -1) {
+        return -1;
+    }
+
+    new_ptr = mmap(NULL, new_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+        m->fd, 0);
+    if (new_ptr == MAP_FAILED) {
+        return -1;
+    }
+
+    munmap(m->ptr, m->size);
+    m->ptr = new_ptr;
+    m->size = new_size;
+
+    return 0;
+#else
+    DWORD size_high;
+    DWORD size_low;
+    HANDLE new_map;
+    void *new_ptr;
+
+    UnmapViewOfFile(m->ptr);
+    CloseHandle(m->map);
+
+    size_high = (DWORD)(new_size >> 32);
+    size_low = (DWORD)(new_size & 0xFFFFFFFF);
+
+    new_map = CreateFileMappingA(m->file, NULL, PAGE_READWRITE,
+        size_high, size_low, NULL);
+    if (!new_map) {
+        size_high = (DWORD)(m->size >> 32);
+        size_low = (DWORD)(m->size & 0xFFFFFFFF);
+        m->map = CreateFileMappingA(m->file, NULL, PAGE_READWRITE,
+            size_high, size_low, NULL);
+        if (m->map) {
+            m->ptr = MapViewOfFile(m->map, FILE_MAP_ALL_ACCESS, 0, 0, m->size);
+        }
+        return -1;
+    }
+
+    new_ptr = MapViewOfFile(new_map, FILE_MAP_ALL_ACCESS, 0, 0, new_size);
+    if (!new_ptr) {
+        CloseHandle(new_map);
+        size_high = (DWORD)(m->size >> 32);
+        size_low = (DWORD)(m->size & 0xFFFFFFFF);
+        m->map = CreateFileMappingA(m->file, NULL, PAGE_READWRITE,
+            size_high, size_low, NULL);
+        if (m->map) {
+            m->ptr = MapViewOfFile(m->map, FILE_MAP_ALL_ACCESS, 0, 0, m->size);
+        }
+        return -1;
+    }
+
+    m->map = new_map;
+    m->ptr = new_ptr;
+    m->size = new_size;
+
+    return 0;
+#endif
+}
+
+/**
  * Initialize a new krow store.
  * @param path File path.
  * @param capacity Maximum number of index slots.
@@ -63,45 +252,51 @@ static void kc_krow_die(const char *msg) {
  */
 kc_krow_t *kc_krow_open(const char *path, uint64_t capacity) {
     kc_krow_t *ctx;
-    struct stat st;
     size_t header_size;
     size_t index_size;
     size_t initial_heap;
     int is_new;
+    size_t map_size;
 
     ctx = calloc(1, sizeof(kc_krow_t));
     if (!ctx) {
         return NULL;
     }
 
-    ctx->fd = open(path, O_RDWR | O_CREAT, 0644);
-    if (ctx->fd == -1) {
-        kc_krow_die("krow: open");
+#ifndef _WIN32
+    struct stat st;
+    if (stat(path, &st) == -1) {
+        is_new = 1;
+        map_size = 0;
+    } else {
+        is_new = (st.st_size == 0);
+        map_size = st.st_size;
     }
-
-    if (fstat(ctx->fd, &st) == -1) {
-        kc_krow_die("krow: fstat");
+#else
+    WIN32_FILE_ATTRIBUTE_DATA st;
+    if (!GetFileAttributesExA(path, GetFileExInfoStandard, &st)) {
+        is_new = 1;
+        map_size = 0;
+    } else {
+        uint64_t sz = ((uint64_t)st.nFileSizeHigh << 32) | st.nFileSizeLow;
+        is_new = (sz == 0);
+        map_size = sz;
     }
+#endif
 
-    is_new = (st.st_size == 0);
     header_size = sizeof(kc_krow_header_t);
     index_size = capacity * sizeof(kc_krow_node_t);
     initial_heap = 1024 * 1024 * 64;
 
     if (is_new) {
-        ctx->map_size = header_size + index_size + initial_heap;
-        if (ftruncate(ctx->fd, ctx->map_size) == -1) {
-            kc_krow_die("krow: ftruncate");
-        }
-    } else {
-        ctx->map_size = st.st_size;
+        map_size = header_size + index_size + initial_heap;
     }
 
-    ctx->header = mmap(NULL, ctx->map_size, PROT_READ | PROT_WRITE,
-        MAP_SHARED, ctx->fd, 0);
-    if (ctx->header == MAP_FAILED) {
-        kc_krow_die("krow: mmap");
+    if (kc_map_open(&ctx->map, path, map_size, is_new) != 0) {
+        kc_krow_die("krow: map open");
     }
+
+    ctx->header = (kc_krow_header_t *)ctx->map.ptr;
 
     if (is_new) {
         ctx->header->magic = KC_KROW_MAGIC;
@@ -129,8 +324,7 @@ void kc_krow_close(kc_krow_t *ctx) {
         return;
     }
 
-    munmap(ctx->header, ctx->map_size);
-    close(ctx->fd);
+    kc_map_close(&ctx->map);
     free(ctx);
 }
 
@@ -145,50 +339,66 @@ void kc_krow_close(kc_krow_t *ctx) {
 int kc_krow_set(kc_krow_t *ctx, uint64_t key, const void *value, size_t size) {
     uint64_t slot;
     uint64_t i;
+    uint64_t target_idx;
+    int found_tombstone;
 
     if (ctx->header->count >= ctx->header->capacity) {
         kc_krow_die("krow: capacity reached");
     }
 
-    if (ctx->header->data_tail + size > ctx->map_size) {
-        size_t new_size = ctx->map_size * 2;
+    if (ctx->header->data_tail + size > ctx->map.size) {
+        size_t new_size = ctx->map.size * 2;
         while (ctx->header->data_tail + size > new_size) {
             new_size *= 2;
         }
 
-        munmap(ctx->header, ctx->map_size);
-        if (ftruncate(ctx->fd, new_size) == -1) {
-            kc_krow_die("krow: ftruncate expansion");
+        if (kc_map_resize(&ctx->map, new_size) != 0) {
+            kc_krow_die("krow: map resize");
         }
 
-        ctx->header = mmap(NULL, new_size, PROT_READ | PROT_WRITE,
-            MAP_SHARED, ctx->fd, 0);
-        if (ctx->header == MAP_FAILED) {
-            kc_krow_die("krow: mmap expansion");
-        }
-
-        ctx->map_size = new_size;
+        ctx->header = (kc_krow_header_t *)ctx->map.ptr;
         ctx->index = (kc_krow_node_t *)((uint8_t *)ctx->header +
             sizeof(kc_krow_header_t));
         ctx->heap = (uint8_t *)ctx->header;
     }
 
-    slot = key % ctx->header->capacity;
+    slot = (key * 11400714819323198485ULL) % ctx->header->capacity;
+    target_idx = 0;
+    found_tombstone = 0;
+
     for (i = 0; i < ctx->header->capacity; i++) {
         uint64_t idx = (slot + i) % ctx->header->capacity;
-        /* Empty or Tombstone */
-        if (ctx->index[idx].length == 0 || ctx->index[idx].length == (uint64_t)-1) {
-            memcpy(ctx->heap + ctx->header->data_tail, value, size);
-            ctx->index[idx].key = key;
-            ctx->index[idx].offset = ctx->header->data_tail;
-            ctx->index[idx].length = size;
-            ctx->header->data_tail += size;
-            ctx->header->count++;
-            return KC_KROW_OK;
+        
+        if (ctx->index[idx].length == 0) {
+            if (!found_tombstone) {
+                target_idx = idx;
+            }
+            break;
+        }
+
+        if (ctx->index[idx].length == (uint64_t)-1) {
+            if (!found_tombstone) {
+                target_idx = idx;
+                found_tombstone = 1;
+            }
         }
     }
 
-    return KC_KROW_ERROR;
+    if (!found_tombstone && ctx->index[target_idx].length != 0) {
+        return KC_KROW_ERROR;
+    }
+
+    memcpy(ctx->heap + ctx->header->data_tail, value, size);
+    ctx->index[target_idx].key = key;
+    ctx->index[target_idx].offset = ctx->header->data_tail;
+    ctx->index[target_idx].length = size;
+    ctx->header->data_tail += size;
+    
+    if (!found_tombstone) {
+        ctx->header->count++;
+    }
+
+    return KC_KROW_OK;
 }
 
 /**
@@ -203,14 +413,13 @@ int kc_krow_get(kc_krow_t *ctx, uint64_t key, kc_krow_cb cb, void *arg) {
     uint64_t slot;
     uint64_t i;
 
-    slot = key % ctx->header->capacity;
+    slot = (key * 11400714819323198485ULL) % ctx->header->capacity;
     for (i = 0; i < ctx->header->capacity; i++) {
         uint64_t idx = (slot + i) % ctx->header->capacity;
         if (ctx->index[idx].length == 0) {
             break;
         }
 
-        /* Skip Tombstones */
         if (ctx->index[idx].length == (uint64_t)-1) {
             continue;
         }
@@ -236,14 +445,15 @@ int kc_krow_del(kc_krow_t *ctx, uint64_t key) {
     uint64_t slot;
     uint64_t i;
 
-    slot = key % ctx->header->capacity;
+    slot = (key * 11400714819323198485ULL) % ctx->header->capacity;
     for (i = 0; i < ctx->header->capacity; i++) {
         uint64_t idx = (slot + i) % ctx->header->capacity;
         if (ctx->index[idx].length == 0) {
             break;
         }
 
-        if (ctx->index[idx].length != (uint64_t)-1 && ctx->index[idx].key == key) {
+        if (ctx->index[idx].length != (uint64_t)-1 &&
+            ctx->index[idx].key == key) {
             ctx->index[idx].length = (uint64_t)-1;
             ctx->header->count--;
         }
@@ -258,9 +468,5 @@ int kc_krow_del(kc_krow_t *ctx, uint64_t key) {
  * @return Status code.
  */
 int kc_krow_sync(kc_krow_t *ctx) {
-    if (msync(ctx->header, ctx->map_size, MS_SYNC) == -1) {
-        return KC_KROW_ERROR;
-    }
-
-    return KC_KROW_OK;
+    return kc_map_sync(&ctx->map);
 }
